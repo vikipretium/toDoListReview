abd73b0eb305864a622685b8a0f7dc4df64beb89
vikipretium <vighneshpasupathy@gmail.com>
2022-02-28 23:02:50 +0530

    fix linters

--- src/asset/modules/statusCheck.js
+++ src/asset/modules/statusCheck.js
@@ -1,12 +1,12 @@
 // status checkbox
 const statusCheckBox = (e, ListContent) => {
   const taskDescription = e.target.nextElementSibling;
-  taskDescription.classList.toggle("strike");
-  const completed = !list.classList.contains("disabled");
-  list.classList.toggle("disabled");
-  const getLists = document.querySelector("#ulList").children;
-  const index = Array.from(getLists).indexOf(list);
+  taskDescription.classList.toggle('strike');
+  const completed = !taskDescription.classList.contains('disabled');
+  taskDescription.classList.toggle('disabled');
+  const getLists = document.querySelector('#ulList').children;
+  const index = Array.from(getLists).indexOf(taskDescription);
   ListContent[index].completed = completed;
-  localStorage.setItem("tasks", JSON.stringify(ListContent));
+  localStorage.setItem('tasks', JSON.stringify(ListContent));
 };
 export default statusCheckBox;
